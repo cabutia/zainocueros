d4a442b4e57c1521682134e3dcb6df8ac14c8b8a
Leandro Gomez <gleandro@ceres.solutions>
2017-06-09 07:49:31 -0300

    Actualizada la estructura de /store

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -3,17 +3,6 @@
 @section('content')
   <header class="relative no-padding">
 
-    <div class="abs-store-wrapper valign-wrapper">
-      <div class="row valign">
-        <div class="col s12 center">
-          <img src="{{ asset('images/zaino-black-logo.png') }}" alt="" class="responsive-img center">
-        </div>
-        <div class="col s12">
-          <h1 class="header-welcome valign">¡Bienvenido a nuestra tienda!</h1>
-        </div>
-      </div>
-    </div>
-
     <!-- Shopping fixed -->
     @include('mainweb.absolute-shopping')
 
@@ -23,11 +12,6 @@
     <!-- Navbar Mobile -->
     @include('home.navbar-mobile')
 
-    <div class="row no-margin header-img">
-      <div class="col s12 no-padding header-img-container">
-        <img src="{{ asset('images/online-store/header-background.jpg') }}" alt="zaino cueros header" class="header-img">
-      </div>
-    </div>
   </header>
 
   <!-- Product landing -->
--- resources/views/store/product-landing.blade.php
+++ resources/views/store/product-landing.blade.php
@@ -3,12 +3,22 @@
   <div class="col s12 m4 l8 product-in-landing brown">
     # Latest product
   </div>
-  <div class="col s12 m4 l4 product-in-landing brown darken-3">
-    # Other latest one
+
+  <div class="col s12 m4 l4 product-in-landing forget-overflow no-padding relative lighten-1">
+    <div class="product-extension">
+      <span class="title flow-text">Lorem ipsum <b>dolor</b></span>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.
+        <a href="#" class="btn cyan go-to-product">ver detalles</a>
+      </p>
+    </div>
+    <div class="parallax-container">
+      <div class="parallax"><img src="{{ asset('images/online-store/products/sample_001.jpg') }}"></div>
+    </div>
   </div>
 
   <!-- General products -->
-  <div class="col s6 m4 l4 product-in-landing forget-overflow no-padding relative lighten-1">
+  <div class="col s12 m4 l4 product-in-landing forget-overflow no-padding relative lighten-1">
     <div class="important-product">
       -25%
     </div>
@@ -16,16 +26,37 @@
       <span class="title flow-text">Lorem ipsum <b>dolor</b></span>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.
+        <a href="#" class="btn cyan go-to-product">ver detalles</a>
       </p>
     </div>
     <div class="parallax-container">
       <div class="parallax"><img src="{{ asset('images/online-store/products/sample_003.jpg') }}"></div>
     </div>
   </div>
-  <div class="col s6 m4 l4 product-in-landing brown lighten-2">
-    # Simple products
+
+  <div class="col s12 m4 l4 product-in-landing forget-overflow no-padding relative lighten-1">
+    <div class="product-extension">
+      <span class="title flow-text">Lorem ipsum <b>dolor</b></span>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.
+        <a href="#" class="btn cyan go-to-product">ver detalles</a>
+      </p>
+    </div>
+    <div class="parallax-container">
+      <div class="parallax"><img src="{{ asset('images/online-store/products/sample_002.jpg') }}"></div>
+    </div>
   </div>
-  <div class="col s6 m4 l4 product-in-landing brown lighten-3">
-    # Simple products
+
+  <div class="col s12 m4 l4 product-in-landing forget-overflow no-padding relative lighten-1">
+    <div class="product-extension">
+      <span class="title flow-text">Lorem ipsum <b>dolor</b></span>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.
+        <a href="#" class="btn cyan go-to-product">ver detalles</a>
+      </p>
+    </div>
+    <div class="parallax-container">
+      <div class="parallax"><img src="{{ asset('images/online-store/products/sample_004.jpg') }}"></div>
+    </div>
   </div>
 </div>
